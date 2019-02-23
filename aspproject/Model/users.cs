b5e33b1d2b57dce5b3f4f1983c4fa19ce1fc14e2
Saud khan <raosaud786@gmail.com>
2019-02-23 11:52:35 +0500

    adding the user model

---
+++ aspproject/Model/users.cs
@@ -0,0 +1,11 @@
+namespace aspproject.Model
+{
+    public class users
+    {
+        public int Id { get; set; }
+        public int Username { get; set; }
+        public byte[] PasswordHash { get; set; }
+        public byte[] PasswordSalt { get; set; }
+
+    }
+}
